5940e4762c35267f897f0f46fc716d4adc6916fe
Nelson Estevão <nelsonmestevao@gmail.com>
2019-02-24 17:15:21 +0000

    [A1S1]: Add notes and projects

---
+++ 2nd/2/Sistemas Operativos/Exercises/guide_2/build/main.d
@@ -0,0 +1,19 @@
+main.o: src/main.c /usr/include/stdc-predef.h /usr/include/stdio.h \
+ /usr/include/bits/libc-header-start.h /usr/include/features.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/wordsize.h \
+ /usr/include/bits/long-double.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/8.2.1/include/stddef.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/8.2.1/include/stdarg.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/types/__fpos_t.h /usr/include/bits/types/__mbstate_t.h \
+ /usr/include/bits/types/__fpos64_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ /usr/include/bits/stdio.h /usr/include/stdlib.h \
+ /usr/include/bits/floatn.h /usr/include/bits/floatn-common.h \
+ /usr/include/bits/stdlib-bsearch.h /usr/include/bits/stdlib-float.h \
+ /usr/include/string.h /usr/include/unistd.h \
+ /usr/include/bits/posix_opt.h /usr/include/bits/confname.h \
+ /usr/include/sys/wait.h /usr/include/bits/waitflags.h \
+ /usr/include/bits/waitstatus.h src/coiso.h
