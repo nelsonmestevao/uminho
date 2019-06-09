13a8066e66a80c073ad72d13951665cba34cdfc5
Nelson Estevão <nelsonmestevao@gmail.com>
2019-06-09 15:58:18 +0100

    [SO]: Cleaning guide 1

--- 2nd/2/Sistemas Operativos/Exercises/guide_1/src/main.c
+++ 2nd/2/Sistemas Operativos/Exercises/guide_1/src/main.c
@@ -5,14 +5,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define SIZE_TO_WRITE 10*1024*1024
+#define SIZE_TO_WRITE 10 * 1024 * 1024
 #define BUFFER_SIZE 100
 #define N 1024
 
 void createFile(const char *path, char ch) {
     int i, fd = open(path, O_CREAT | O_TRUNC | O_WRONLY, 0777);
     char buf[BUFFER_SIZE];
-
 
     for (i = 0; i < BUFFER_SIZE; i++) {
         buf[i] = ch;
@@ -32,7 +31,7 @@ void mycat() {
     do {
         res = read(0, &byte, 1);
         write(1, &byte, 1);
-    } while(res != 0);
+    } while (res != 0);
 }
 
 void mycat2() {
@@ -42,7 +41,7 @@ void mycat2() {
     do {
         res = read(0, buf, BUFFER_SIZE);
         write(1, buf, res);
-    } while(res != 0);
+    } while (res != 0);
 }
 
 ssize_t readln(int fildes, void *buf, size_t nbyte) {
@@ -50,7 +49,7 @@ ssize_t readln(int fildes, void *buf, size_t nbyte) {
     int i = 0;
 
     while (i < nbyte && (res += read(fildes, buf + i, 1) > 0)) {
-        if (((char *) buf)[i] == '\n') {
+        if (((char *)buf)[i] == '\n') {
             return i;
         }
 
@@ -65,7 +64,6 @@ ssize_t readln(int fildes, void *buf, size_t nbyte) {
  * @param argc quantos argumentos
  * */
 int main(int argc, char **argv) {
-
     if (argc < 1) {
         printf("Unexpected number of arguments\n");
         return -1;
@@ -81,4 +79,3 @@ int main(int argc, char **argv) {
 
     return 0;
 }
-
