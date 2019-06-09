3ba367ab4a5cbbb982fe4050462080a241cfd0ed
Nelson Estevão <nelsonmestevao@gmail.com>
2019-06-09 16:00:37 +0100

    [SO]: Cleaning guide 2

--- 2nd/2/Sistemas Operativos/Exercises/guide_2/src/main.c
+++ 2nd/2/Sistemas Operativos/Exercises/guide_2/src/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 /**
  * Importante
@@ -13,11 +13,11 @@ int main(int argc, char **argv) {
 
     if ((pid = fork()) == 0) {
         printf("Este é o processo filho: L1\n");
-        printf("Indentificador do processo L1:\t%d\n",       getpid());
+        printf("Indentificador do processo L1:\t%d\n", getpid());
         printf("Identificador do pai do processo L1:\t%d\n", getppid());
     } else {
         printf("Este é o processo pai: L0\n");
-        printf("Indentificador do processo L0:\t%d\n",       getpid());
+        printf("Indentificador do processo L0:\t%d\n", getpid());
         printf("Identificador do pai do processo L0:\t%d\n", getppid());
     }
 
