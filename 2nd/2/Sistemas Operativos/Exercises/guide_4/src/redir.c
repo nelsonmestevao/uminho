acdef13c469a90e31eb031e412d5914f17048d45
Nelson Estevão <nelsonmestevao@gmail.com>
2019-06-09 16:03:16 +0100

    [SO]: Cleaning guide 4

--- 2nd/2/Sistemas Operativos/Exercises/guide_4/src/redir.c
+++ 2nd/2/Sistemas Operativos/Exercises/guide_4/src/redir.c
@@ -4,13 +4,13 @@
 #include <unistd.h>
 
 int main() {
-  int ifd = open("/etc/passwd", O_RDONLY);
-  int ofd = open("saida.txt", O_CREAT | O_TRUNC | O_WRONLY, 0666);
-  int efd = open("erros.txt", O_CREAT | O_TRUNC | O_WRONLY, 0666);
+    int ifd = open("/etc/passwd", O_RDONLY);
+    int ofd = open("out/saida.txt", O_CREAT | O_TRUNC | O_WRONLY, 0666);
+    int efd = open("out/erros.txt", O_CREAT | O_TRUNC | O_WRONLY, 0666);
 
-  printf("ifd = %d\n", ifd);
-  printf("ofd = %d\n", ofd);
-  printf("efd = %d\n", efd);
+    printf("ifd = %d\n", ifd);
+    printf("ofd = %d\n", ofd);
+    printf("efd = %d\n", efd);
 
-  return 1;
+    return 1;
 }
